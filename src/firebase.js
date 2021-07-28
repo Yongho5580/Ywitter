576ae3a02779fbde36f16696892217754c63cf71
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-07-28 15:13:55 +0900

    Init #1.1

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJZiImR1i1-6a7gihvMgvtNcy-VllvCIs",
+  authDomain: "ywitter-4a507.firebaseapp.com",
+  projectId: "ywitter-4a507",
+  storageBucket: "ywitter-4a507.appspot.com",
+  messagingSenderId: "664821589623",
+  appId: "1:664821589623:web:805af462305bb1d985c872",
+};
+
+export default firebase.initializeApp(firebaseConfig);
