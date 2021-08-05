ec88bed87f4b75b9f77f6ebc2dde175fdfe29281
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-08-05 16:49:10 +0900

    ADD Loading bar

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import AppRouter from "./Router";
 import { authService } from "../fbase";
 import "../css/App.css";
+import { BarLoader } from "react-spinners";
 
 function App() {
   // init 이 false 이면 AppRouter 를 hidden 시키기 위해서 추가한 state 값이다. 즉, useEffect 를 통해
@@ -45,7 +46,7 @@ function App() {
           userObj={userObj}
         />
       ) : (
-        "Initializing..."
+        <BarLoader loading color="#08AAFF" />
       )}
     </>
   );
